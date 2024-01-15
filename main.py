efa5481fd4a934bbc9491b60ef1bb6a046616fa4
Erozist <Erozist87@gmail.com>
2024-01-15 22:42:35 +0200

    commit1

---
+++ main.py
@@ -0,0 +1,69 @@
+from datetime import date, datetime, timedelta
+
+
+def get_birthdays_per_week(users):
+    # Реалізуйте тут домашнє завдання
+    date_today = date.today()
+    weekend = date_today + timedelta(days=7)
+    previos_day = date_today - timedelta(days=1)
+    previos_2day = date_today - timedelta(days=2)
+    dict_birthday = {}
+    
+    # Перевірка чи пустий список
+    if len(users) < 1:
+        return {}
+    
+    # Проходимо циклом по списку працівників
+    for user in users:
+ 
+    # День нардження працівника
+        birthday = user.get('birthday').replace(year=date_today.year)
+        birthday_new_year = user.get('birthday').replace(year=date_today.year+1)
+ 
+    # Імя працівника
+        name_user = user.get('name')
+ 
+    # День тиждня
+        day_week = birthday.strftime('%A')
+  
+    # Перевіряємо чи на цьому тиждні є день народження в новому році
+        if birthday_new_year < weekend:
+            birthday = birthday_new_year
+ 
+    # Певевіряємо якщо сьогодні Понеділок, чи були дні народження на вихідних   
+        if date_today.strftime('%A') == 'Monday':
+            if birthday == previos_day or birthday == previos_2day:
+                dict_birthday.update({'Monday': [name_user]})
+        
+    # Відкидаємо дні народження які вже пройшли
+        if birthday < date_today:
+            continue
+  
+    # Відкидаємо дні народження які в майбутньому
+        if weekend < birthday:
+            continue
+ 
+    # Певевіряємо чи день народження не в Суботу та Неділю
+        if day_week == 'Saturday' or day_week == 'Sunday':
+            day_week = 'Monday'
+  
+    # Певевіряємо чи є  всловнику запис
+        if day_week not in dict_birthday:
+            dict_birthday.update({day_week:[]})
+   
+    # Додаємо в словник дні народження
+        dict_birthday[day_week].append(name_user)
+
+    return dict_birthday
+
+
+if __name__ == "__main__":
+
+    users = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},]
+
+    result = get_birthdays_per_week(users)
+    print(result)
+    # Виводимо результат
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
